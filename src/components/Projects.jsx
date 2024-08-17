f6fd23b5f5aeaffc62079934fbf72bea8782a55b
Abhhirram Veedhi <141173291+abhhirramVeedhi@users.noreply.github.com>
2024-08-18 05:16:44 +0530

    updated  Projects.jsx as 3rd project doest shows up the title and navigation to github

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,7 +21,7 @@ const Projects = () => {
             transition={{ duration: 0.5 }}
             whileHover={{ scale: 1.05 }}
             key={project.id}
-            className="group relative overflow-hidden rounded-3xl"
+            className="group relative overflow-hidden rounded-3xl min-h-[400px]" // Increased min height
           >
             <motion.img
               whileHover={{ scale: 1.1 }}
@@ -35,19 +35,21 @@ const Projects = () => {
               transition={{ duration: 0.5 }}
               className="absolute inset-0 flex flex-col items-center justify-center text-white bg-black bg-opacity-50 transition-opacity duration-500 lg:bg-opacity-50 lg:opacity-0 lg:group-hover:opacity-100"
             >
-              <h3 className="mb-2 text-3xl font-extrabold">{project.name}</h3>
-              <p className="mb-12 p-4 text-center text-m2xl font-bold">
+              <h3 className="mb-6 text-center text-2xl font-bold lg:text-3xl">
+                {project.name}
+              </h3>
+              <p className="mb-8 px-4 text-center text-lg font-semibold lg:text-xl">
                 {project.description}
               </p>
               <a
                 href={project.githubLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300"
+                className="absolute bottom-4 rounded-full bg-white px-3 py-1 text-sm text-black hover:bg-gray-300"
               >
                 <div className="flex items-center font-bold">
                   <span>View on GitHub</span>
-                  <MdArrowOutward />
+                  <MdArrowOutward className="ml-1" />
                 </div>
               </a>
             </motion.div>
